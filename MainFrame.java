e90f137e023c63b6b2e99132d6542d7a7088deaa
usernameisnot1234 <2022986@apps.nsd.org>
2025-06-17 07:38:18 -0700

    Drawing fix

--- MainFrame.java
+++ MainFrame.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class MainFrame extends JFrame{
-    public DrawPanel d;
+    public DrawPanel drawPanel;
     public static void run() throws InterruptedException{
         MainFrame gui = new MainFrame();
         SwingUtilities.invokeLater(() -> gui.createFrame(gui));
@@ -14,8 +14,8 @@ public class MainFrame extends JFrame{
     public void createFrame(Object semaphore){
         this.setSize(500, 500);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        d = new DrawPanel(this);
-        d.setVisible(true);
+        drawPanel = new DrawPanel(this);
+        add(drawPanel);
         this.setVisible(true);
         synchronized (semaphore) {
             semaphore.notify();
